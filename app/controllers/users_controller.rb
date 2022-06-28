27e1a60bd95d450412cc875a5c59fadb24eb9f2a
smtn.roman <smtn.roman@gmail.com>
2022-06-28 09:31:29 +0200

    Added Edit to header | Updating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
-  def index 
-    @user = current_user
-  end
+  before_action :set_user
+  before_action :user_params, only: [ :update ]
+  def index; end
 
   def edit; end
 
-  def update; end
+  def update
+    if @user.update(user_params)
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 end
