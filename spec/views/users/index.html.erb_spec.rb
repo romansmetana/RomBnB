a779cf1992aa0692c4b332cb36bf873e7a0d6f9e
smtn.roman <smtn.roman@gmail.com>
2022-06-29 17:43:18 +0200

    Index view test

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'users/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    
+    @user = assign(:user, User.create!(
+      email: 'test@test.com',
+      password: 'password',
+      first_name: 'Alex',
+      last_name: 'Duoling'))
+
+  end
+
+  it "render main page" do
+    render template: 'users/index'
+    expect(rendered).to match(/Glad to see you Alex Duoling in RomBnB/)  
+  end
 end
