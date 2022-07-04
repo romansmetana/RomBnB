ee929b7a48f917347de3a4b80c420a9577ed1139
smtn.roman <smtn.roman@gmail.com>
2022-07-04 12:26:46 +0200

    Style for main hotel page

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,9 +1,15 @@
 class HotelsController < ApplicationController
+  before_action :set_hotel, only: [:show]
   def index
     @hotels = Hotel.all
     @hotels_count = Hotel.count
   end
 
   def show
+  end
+  private
+
+  def set_hotel
+    @hotel = Hotel.find(params[:id])
   end
 end
