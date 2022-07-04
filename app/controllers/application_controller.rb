f4a010b25ca7846ace66e99fa332d4d5c0dc6e20
smtn.roman <smtn.roman@gmail.com>
2022-07-04 08:21:04 +0200

    Created Hotels controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_user
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   add_flash_types :danger, :info, :warning, :success
 
   protected
+  def set_user
+    @user = current_user
+  end
+  
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'hotels/index'
+  get 'hotels/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index edit update]
   # Defines the root path route ("/")
-  root to: 'users#index'
+  root to: 'hotels#index'
 end
