b5f1d45e50615ee8378fdfa77aeea3d7bbd032e7
smtn.roman <smtn.roman@gmail.com>
2022-07-05 09:34:46 +0200

    code fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'hotels/index'
-  get 'hotels/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index edit update]
+  resources :hotels
   # Defines the root path route ("/")
   root to: 'hotels#index'
 end
