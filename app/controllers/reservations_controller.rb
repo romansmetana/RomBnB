02d5b09a17c46fb9037039d7b199dbb4a05806da
smtn.roman <smtn.roman@gmail.com>
2022-07-06 13:58:55 +0200

    rubocop fix

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,39 +1,38 @@
 class ReservationsController < ApplicationController
-    before_action :set_reserv, only: %i[ destroy ]
-    def index
-        @reservations = Resrvation.all
-    end
-    
-    def create
-        if session[:reservation_id]
-            flash[:warning] = "You have already booked"
-            redirect_to reservations_path
-        else
-            @reservation = Resrvation.create(reserv_params)
-             if @reservation.save
-                session[:reservation_id] = @reservation.id
-                redirect_to root_path
-                flash[:success] = "Your reservation was created"
-            end
-        end
-    end
+  before_action :set_reserv, only: %i[destroy]
+  def index
+    @reservations = Resrvation.all
+  end
 
-    def destroy
-        if @reservation.destroy
-            session[:reservation_id] = nil
-            redirect_to root_path
-            flash[:danger] = "You have canceled your reservation"
-        end
+  def create
+    if session[:reservation_id]
+      flash[:warning] = 'You have already booked'
+      redirect_to reservations_path
+    else
+      @reservation = Resrvation.create(reserv_params)
+      if @reservation.save
+        session[:reservation_id] = @reservation.id
+        redirect_to root_path
+        flash[:success] = 'Your reservation was created'
+      end
     end
-    
+  end
 
-    private
-    def set_reserv
-        @reservation = Resrvation.find(params[:id])
+  def destroy
+    if @reservation.destroy
+      session[:reservation_id] = nil
+      redirect_to root_path
+      flash[:danger] = 'You have canceled your reservation'
     end
-    
+  end
 
-    def reserv_params 
-        params.permit(:user_id, :room_id)
-    end
+  private
+
+  def set_reserv
+    @reservation = Resrvation.find(params[:id])
+  end
+
+  def reserv_params
+    params.permit(:user_id, :room_id)
+  end
 end
