c299f2916a6299200b0d7b5bb87bbeb3d1d8d413
smtn.roman <smtn.roman@gmail.com>
2022-07-06 11:47:45 +0200

    session control for method create | alerts

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,18 +5,27 @@ class ReservationsController < ApplicationController
     end
     
     def create
-        @reservation = Resrvation.create(reserv_params)
-        if @reservation.save
-            session[:reservation_id] = @reservation.id
-            redirect_to root_path
-            flash[:success] = "Your reservation was created"
+        if session[:reservation_id]
+            
+            binding.pry
+            
+            flash[:warning] = "You have already booked"
+            redirect_to reservations_path
+        else
+            @reservation = Resrvation.create(reserv_params)
+             if @reservation.save
+                session[:reservation_id] = @reservation.id
+                redirect_to root_path
+                flash[:success] = "Your reservation was created"
+            end
         end
     end
 
     def destroy
         if @reservation.destroy
             session[:reservation_id] = nil
-            flash[:danger] = "Your reservation was canceled"
+            redirect_to root_path
+            flash[:danger] = "You have canceled your reservation"
         end
     end
     
