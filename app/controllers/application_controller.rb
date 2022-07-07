5ff277fc97c4450aa5e2f22314ef5b52437cbb97
smtn.roman <smtn.roman@gmail.com>
2022-07-07 12:14:01 +0200

    Registration as an owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :owner)
     end
   end
 end
