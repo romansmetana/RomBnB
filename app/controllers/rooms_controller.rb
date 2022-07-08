eb689517df2cf5970d084b2536d3c0168e740b75
smtn.roman <smtn.roman@gmail.com>
2022-07-08 16:08:26 +0200

    Added new/create methods for room controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,22 @@
 class RoomsController < ApplicationController
+    def new
+        @hotel = Hotel.find(params[:hotel_id])
+        @room = Room.new
+    end
+
+    def create
+        @room = Room.create(room_params)
+        if @room.save
+            flash[:success] = 'Room was successfuly created'
+            redirect_to root_path
+        else
+            flash[:danger] = 'Something wrong..'
+            redirect_to root_path
+        end
+    end
+    private
+
+    def room_params
+       params.permit(:name, :price, :square, :capacity, :count, :double_bed, :single_bed, :hotel_id) 
+    end
 end
