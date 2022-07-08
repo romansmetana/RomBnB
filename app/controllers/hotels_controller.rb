102f0161e2ea9b9f748f791adfcfe8b5e850f1b2
smtn.roman <smtn.roman@gmail.com>
2022-07-08 12:05:10 +0200

    Added link_to add hotel to header, created method new

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,11 +1,20 @@
 class HotelsController < ApplicationController
-  before_action :set_hotel, only: [:show]
+  before_action :set_hotel, only: %i[ show ]
   def index
     @hotels = policy_scope(Hotel).all
     @hotels_count = policy_scope(Hotel).count
   end
 
   def show; end
+
+  def new
+    @hotel = Hotel.new
+  end
+
+  def create
+    
+  end
+  
 
   private
 
