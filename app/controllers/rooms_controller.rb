e01f8a94af8c629c52acef5e3afc3558af9fcfb9
smtn.roman <smtn.roman@gmail.com>
2022-07-09 12:14:36 +0200

    Created edit/update methods

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
-    before_action :set_hotel, only: %i[ new create ]
+    before_action :set_hotel, only: %i[ new create edit update ]
+    before_action :set_room, only: %i[edit update]
     def new
         @room = Room.new
     end
@@ -14,13 +15,36 @@ class RoomsController < ApplicationController
             redirect_to root_path
         end
     end
+
+    def edit
+
+    end
+
+    def update
+        if @room.update(room_params_require)
+            flash[:success] = 'Room was successfuly updated'
+            redirect_to root_path
+        else
+            flash[:danger] = 'Something wrong..'
+            redirect_to root_path
+        end
+    end
+
     private
+    def set_room
+        @room = @hotel.rooms.find(params[:id])
+    end
+    
 
     def set_hotel
         @hotel = Hotel.find(params[:hotel_id])
     end
     
     def room_params
-       params.permit(:name, :price, :square, :capacity, :count, :double_bed, :single_bed, :hotel_id) 
+        params.permit(:name, :price, :square, :capacity, :count, :double_bed, :single_bed, :hotel_id) 
+     end
+
+    def room_params_require
+       params.require(:room).permit(:name, :price, :square, :capacity, :count, :double_bed, :single_bed, :hotel_id) 
     end
 end
