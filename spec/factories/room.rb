9e7e0d60777c3e994126bd8e51c819b2ee83d09c
smtn.roman <smtn.roman@gmail.com>
2022-07-12 10:21:45 +0200

    Rspec: room model test

---
+++ spec/factories/room.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+    factory :room do
+      name { 'For two person' }
+      price { '130.60' }
+      square { '40' }
+      capacity { '2' }
+      count { '7' }
+      double_bed { '' }
+      single_bed { '2' }
+      hotel_id { '' }
+    end
+end
+  
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+    describe '#validation' do
+        let(:user) { create(:user) }
+        let(:hotel) { create(:hotel, user_id: user.id) }
+        let(:room) { create(:room, hotel_id: hotel.id) } 
+    
+        it 'valid room' do
+          expect(room).to be_valid
+        end
+
+        it "invalid room" do
+            room.name = ''
+            room.price = ''
+            room.square = ''
+            room.capacity = ''
+            room.count = ''
+            room.hotel_id = ''    
+            expect(room).not_to be_valid
+        end
+        
+    end
 end
