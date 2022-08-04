68e85068bf8a242e0e4023b8967f29c1d21cc2e1
smtn.roman <smtn.roman@gmail.com>
2022-08-04 10:29:34 +0200

    hotel's seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,18 @@ tag2 = Label.create(title: 'Parking', icon: 'fa-solid fa-square-parking')
 tag3 = Label.create(title: 'Spa', icon: 'fa-solid fa-spa')
 tag4 = Label.create(title: 'Gym', icon: 'fa-solid fa-dumbbell')
 tag5 = Label.create(title: 'Pets', icon: 'fa-solid fa-paw')
+
+Hotel.delete_all
+
+hotel = Hotel.create(name: 'Hilton',
+                     description: 'Cozy rooms in a modern hotel with a rooftop restaurant and lounge, as well as an indoor pool and spa.', 
+                     address: 'Pobřežní 311/1', 
+                     city: 'Praha', 
+                     user_id: 1)
+room = Room.create(name: 'For two person',
+                   price: 130,
+                   square: 50,
+                   capacity: 2,
+                   count: 5,
+                   double_bed: 1,
+                   hotel_id: 1)
