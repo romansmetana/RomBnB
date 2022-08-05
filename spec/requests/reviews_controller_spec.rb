60f16c0940e95b9798774e71b0467e973096365b
smtn.roman <smtn.roman@gmail.com>
2022-08-05 18:32:40 +0200

    rspec

--- spec/requests/reviews_controller_spec.rb
+++ spec/requests/reviews_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsController, type: :controller do
-  let(:user_owner) { create(:user, owner: true) }
-  before { sign_in(user_owner) }
+  let(:user) { create(:user) }
+  let(:user_owner) { create(:user, email: "owner@gmail.com", owner: true) }
+  before { sign_in(user) }
+
   let(:hotel) { create(:hotel, user_id: user_owner.id) }
   let(:room) { create(:room, hotel_id: hotel.id) }
-  let(:review) { create(:review, user_id: user_owner.id, room_id: room.id) }
+  let(:reservation) { create(:reservation, room_id: room.id, user_id: user.id) } 
+  let(:review) { create(:review) }
 
   describe 'GET /new' do
     it 'returns http success' do
@@ -18,12 +21,17 @@ RSpec.describe ReviewsController, type: :controller do
     let(:params) do
       { review:
         { comment: 'Hiltonojoppo',
-          rating: 4,
-          user_id: user_owner.id,
-          room_id: room.id } }
+          rating: 4, 
+          user_id: user.id,
+          room_id: room.id} }
     end
-    let(:invalid_params) { { hotel: { comment: '', rating: '', user_id: nil, room_id: nil } } }
+    let(:invalid_params) do
+      { review:
+        { comment: 'Hilvartonojoppo',
+          rating: nil } }
+    end
     it 'creates success' do
+
       post :create, params: params
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(root_path)
